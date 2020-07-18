65f61f2237c0eb7375b11a5b21b8e9acc0b190e6
Volodymyr Rusynov <vrusynov@gmail.com>
2020-07-18 08:55:47 +0300

    Routing (#1)
    * done
    
    * deleted pages/api

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,17 +1,22 @@
-import { Layout } from "../components/layout";
-import PostList from "../components/postList";
+import { Layout } from "../../components/layout";
 import { GetServerSideProps } from "next";
 import axios from "axios";
+import { db } from "../index";
 
-export const db: string = 'https://nextjs-challenge.firebaseio.com/posts'
-
-export default function Index({ posts }) {
+function PostById({ post }) {
+  console.log(post)
+  const { title, text } = post;
   return (
-    <>
-      <Layout title="Latest Posts">
-        <h1>The latest posts</h1>
-        <PostList posts={posts}/>
-        <style jsx>{`
+    <Layout>
+      <h1>
+        Title: {title}
+      </h1>
+      <div>
+        <p>
+          {text || 'none'}
+        </p>
+      </div>
+      <style jsx>{`
           h1 {
             color: #242526;
             background: #efeffe;
@@ -20,7 +25,7 @@ export default function Index({ posts }) {
             width: 50vw;
             border-radius: 5px;
           }
-            div {
+          div {
             color: #242526;
             min-width: 390px;
             width: 50vw;
@@ -29,18 +34,32 @@ export default function Index({ posts }) {
             border-radius: 5px;
             margin-bottom: 10px;
           }
+          p {
+            font-size: 18px;
+            margin: 0;
+          }
         `}</style>
-      </Layout>
-    </>
+    </Layout>
   )
+}
+
+interface Post {
+  id: string,
+  text: string,
+  date: string,
 }
 
 export const getServerSideProps: GetServerSideProps = async context => {
   const response = await axios.get(db + '.json');
   const posts = Object.values(response.data);
+  const index = posts.map((post: Post) => post.id).indexOf(context.query.id.toString());
+  const post = posts[index];
+  console.log(post);
   return {
     props: {
-      posts
+      post
     }
   }
 }
+
+export default PostById;
